a5372b2903f4333cc29d3b1c470472ff026dddb9
Ben-Dawit <dawitpieliker@gmail.com>
2022-05-20 00:19:28 -0400

    initial commit

---
+++ server/api/_helpers/db.js
@@ -0,0 +1,40 @@
+const config = require('api/config.js');
+const mysql = require('mysql2/promise');
+const { Sequelize } = require('sequelize');
+
+module.exports = db = {};
+
+initialize();
+
+async function initialize() {
+    
+    try{
+        // create db if it doesn't already exist
+        const { host, port, user, password, database } = config.database;
+        const connection = await mysql.createConnection({ host, port, user, password });
+        await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+        // connect to db
+        const sequelize = new Sequelize(database, user, password, { 
+            host: config.database.host,
+            dialect: 'postgre' 
+        });
+
+        // init models and add them to the exported db object
+        db.Account = require('../accounts/account.model')(sequelize);
+        db.RefreshToken = require('../accounts/refresh-token.model')(sequelize);
+        db.Pantry = require('../food/pantry.model')(sequelize);
+
+        // define relationships
+        db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
+        db.RefreshToken.belongsTo(db.Account);
+        
+        db.Account.hasOne(db.Pantry, { onDelete: 'CASCADE' });
+        db.Pantry.belongsTo(db.Account);
+        // sync all models with database
+        await sequelize.sync();
+    }
+    catch{
+        
+    }
+}
---
+++ server/api/config.js
@@ -0,0 +1,27 @@
+﻿require('dotenv').config();
+
+
+const config = {
+    "database": {
+        "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
+        "user": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE
+    },
+    "secret": process.env.SECRET,
+    "emailFrom": process.env.EMAIL,
+
+    
+    "smtpOptions": {
+        "host": "smtp.gmail.com",
+        "port": 587,
+        "secure": false,
+        "auth": {
+            "user": process.env.EMAIL,
+            "pass": process.env.EMAIL_PASS
+        }
+    },
+
+};
+module.exports = config;
