5cf3162b63450d4c907952411d18efc9e68e9106
Ben-Dawit <dawitpieliker@gmail.com>
2022-05-23 22:08:57 -0400

    setting up for s3

--- server/api/news/news.controller.js
+++ server/api/news/news.controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
 const validateRequest = require('api/_middleware/validate-request');
-const authorize = require('api/_middleware/authorize')
+const authorize = require('api/_middleware/authorize');
 const Role = require('api/_helpers/role');
 const newsService = require('./news.service');
 
@@ -10,8 +10,16 @@ const newsService = require('./news.service');
 //note: all routes begin with /accounts. 
 //EX: https://url.com/accounts/authenticate
 router.get('/scrape',  scrape);
+router.get('/test', test);
 
 module.exports = router;
+
+function test(req,res,next){
+    res.json({
+        message: "test successful!"
+    })
+    .catch(next);
+}
 function scrape(req, res, next) {
     newsService.scrapeNews()
         .then(() => {
