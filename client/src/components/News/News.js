ba913730d01f91f5fa53cc9d88d7ccd84bbc631d
Ben-Dawit <dawitpieliker@gmail.com>
2022-06-03 22:11:27 -0400

    added boilerplate image list to news (prep for news component)

--- client/src/components/News/News.js
+++ client/src/components/News/News.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import Images from './Images/Images'
+import useStyles from './styles'
 
 const News = () =>{
+
+    const classes = useStyles();
+
     return (
-        <Images />
+        <div>
+            <h1>News</h1>
+            <ImageList sx={{ width: 960, height: 820 }} cols={2} rowHeight={164} variant='standard'>
+                {itemData.map((item) => (
+                    <ImageListItem key={item.img}>
+                    <img
+                        src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                        srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                        alt={item.title}
+                        loading="lazy"
+                    />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+        </div>
     )
 }
 
