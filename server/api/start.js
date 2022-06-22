0a339f839c5d751af667626dbbf48f47dde384d5
Ben-Dawit <dawitpieliker@gmail.com>
2022-06-22 14:55:15 -0400

    scheduling a job to work once every day at midnight to scrape news and save it to the images file

--- server/api/start.js
+++ server/api/start.js
@@ -1,4 +1,8 @@
 const app = require ("./server.js");
+const newsService = require("./news/news.service");
+const schedule = require ("node-schedule");
 
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 app.listen(port, () => console.log('Server listening on port ' + port));
+
+const job = schedule.scheduleJob('0 0 0 * *', newsService.scrapeNews );
